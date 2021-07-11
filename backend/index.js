4be6fc9aa666ec81fc223d6214921f01bb3d8021
Brunda Rao <brunda.r.1701@gmail.com>
2021-07-11 23:40:27 +0530

    Location updation

--- backend/index.js
+++ backend/index.js
@@ -2,8 +2,24 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const mongoose = require("mongoose");
-const Logs = require('./src/model/LogSchema'); 
+const nodemailer = require('nodemailer');
+const Logs = require('./src/model/LogSchema');
+const UserLocation = require('./src/model/UserLocationSchema');
 const PORT = 4000;
+
+const mail = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: 'clbemonitor@gmail.com',
+        pass: 'clbem2021'
+    }
+});
+
+const mailOptions = {
+    from: 'clbemonitor@gmail.com',
+    subject: 'Caution: Suspicious Login',
+    html: `<h2>Hello</h2>`
+}
 
 app.use(cors());
 
@@ -13,6 +29,7 @@ mongoose.connect("mongodb+srv://BrundaB5:BrundaB5@123@cluster0.vw63z.mongodb.net
 });
 
 const connection = mongoose.connection;
+mongoose.set('useFindAndModify', false);
 
 connection.once("open", function() {
   console.log("Connection with MongoDB was successful");
@@ -35,6 +52,93 @@ app.post('/', (req, res) => {
       .then(book => res.json({ msg: 'Logs added successfully' }))
       .catch(err => res.status(400).json({ error: 'Unable to log activity' }));
   });
+
+app.post('/location', async (req, res) => {
+    console.log(JSON.stringify(req.body, null, 2));
+
+    const deg2rad = (deg) => (deg * (Math.PI/180));
+
+    const getDistanceFromLatLonInKm = (lat1,lon1,lat2,lon2) => {
+        const R = 6371; // Radius of the earth in km
+        let dLat = deg2rad(lat2-lat1);  // deg2rad below
+        let dLon = deg2rad(lon2-lon1);
+        let a =
+            Math.sin(dLat/2) * Math.sin(dLat/2) +
+            Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) *
+            Math.sin(dLon/2) * Math.sin(dLon/2)
+        ;
+        let c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+        return  R * c; // Distance in km
+    };
+
+    if (req.body.email && req.body.latitude && req.body.longitude) {
+        const new_lat = req.body.latitude;
+        const new_lng = req.body.longitude;
+        let oldLocation;
+        try {
+            oldLocation = await UserLocation.findOne(
+                {email: req.body.email},
+                {_id: 0, email: 1, latitude: 1, longitude: 1, display_name: 1},
+                {lean: true}
+            );
+        } catch(e) {
+            console.log(`Failed to retrieve user location: ${JSON.stringify(e, null, 2)}`);
+            return res.status(400).json({
+                msg: 'Unable to retrieve user location',
+                error: e
+            });
+        }
+        let distance;
+        console.log(`old location: ${JSON.stringify(oldLocation, null, 2)}`);
+        if (oldLocation && oldLocation.latitude && oldLocation.longitude) {
+            const old_lat = oldLocation.latitude;
+            const old_lng = oldLocation.longitude;
+            if (old_lat && old_lng && new_lat && new_lng) {
+                distance = getDistanceFromLatLonInKm(old_lat, old_lng, new_lat, new_lng);
+            }
+            if (distance && distance > 5) {
+                mailOptions.to = req.body.email;
+                mailOptions.html = `<h2>Currently logged in from a place at a distance of ${distance} km from previous login</h2>
+<p>Contact support if you have not logged in</p>`;
+                mail.sendMail(mailOptions, function(error, info){
+                    if (error) {
+                        console.log(error);
+                    } else {
+                        console.log('Email sent: ' + info.response);
+                    }
+                });
+            }
+            console.log(`User logged in from a distance of ${distance} km`);
+        } else {
+            console.log('No user location information found');
+        }
+
+        const query = { email: req.body.email };
+        let update = {
+            latitude: req.body.latitude,
+            longitude: req.body.longitude
+        };
+        if (req.body.display_name)
+            update.display_name = req.body.display_name;
+        const options = {
+            upsert: true,
+            new: true,
+            setDefaultsOnInsert: true
+        };
+
+        console.log(`Update information: ${JSON.stringify(update, null, 2)}`);
+        try {
+            await UserLocation.findOneAndUpdate(query, update, options);
+            console.log('Updated location information successfully');
+            res.status(200).json({ msg: 'Updated location information successfully' });
+        } catch(e) {
+            return res.status(400).json({
+                msg: 'Unable to update user location',
+                error: e
+            });
+        }
+    }
+});
 
 app.listen(PORT, function() {
   console.log("Server is running on Port: " + PORT);
--- frontend/client/src/components/profile.js
+++ frontend/client/src/components/profile.js
@@ -8,21 +8,81 @@ import {auth} from "../firebase";
 
 class Profile extends Component {
     constructor(props) {
-      super(props);
-      this.state = {
-          logs: []
-      };
-      this.addLog = this.addLog.bind(this);
-      this.showLogs = this.showLogs.bind(this);
+        super(props);
+        this.state = {
+            logs: []
+        };
+        this.addLog = this.addLog.bind(this);
+        this.showLogs = this.showLogs.bind(this);
+        const { displayName, email } = this.props.user;
+
+        const success = (pos) => {
+            const lat = pos.coords.latitude;
+            const lng = pos.coords.longitude;
+            const data = {
+                email,
+                display_name: displayName,
+                latitude: lat,
+                longitude: lng
+            };
+
+            fetch('http://localhost:4000/location', {
+                method: 'POST',
+                mode: 'cors',
+                headers: {
+                    'content-type': 'application/json'
+                },
+                body: JSON.stringify(data)
+            });
+
+            console.log("Your current position is:");
+            console.log(`Latitude : ${lat}`);
+            console.log(`Longitude: ${lng}`);
+            console.log(`More or less ${pos.coords.accuracy} meters.`);
+        }
+
+        const errors = (err) => {
+            console.warn(`ERROR(${err.code}): ${err.message}`);
+        }
+
+        const options = {
+            enableHighAccuracy: true,
+            timeout: 5000,
+            maximumAge: 0,
+        };
+
+        const getGeoInformation = () => {
+            if (navigator.geolocation) {
+                navigator.permissions
+                    .query({ name: "geolocation" })
+                    .then(function (result) {
+                        if (result.state === "granted") {
+                            console.log(result.state);
+                            //If granted then you can directly call your function here
+                            navigator.geolocation.getCurrentPosition(success);
+                        } else if (result.state === "prompt") {
+                            navigator.geolocation.getCurrentPosition(success, errors, options);
+                        } else if (result.state === "denied") {
+                            //If denied then you have to show instructions to enable location
+                        }
+                        result.onchange = function () {
+                            console.log(result.state);
+                        };
+                    });
+            } else {
+                console.log("Sorry, Geolocation information Not available!");
+            }
+        }
+        getGeoInformation();
     }
 
-     addLog() {
+    addLog() {
         const { displayName } = this.props.user;
         let data = {
             name: displayName
         };
-        
-    
+
+
         fetch('http://localhost:4000', {
             method: 'POST',
             mode: 'cors',
@@ -48,26 +108,26 @@ class Profile extends Component {
                     be viewed by their respective Users.
                 </h1>
                 <div className="button1">
-                <Button variant="contained" color="primary" onClick = {this.addLog}>
-                    Button X
-                </Button>
-                    </div>
-                <div className="button2">
-                <Button variant="contained" color="primary" onClick = {this.showLogs}>
-                    Log History
-                </Button>
+                    <Button variant="contained" color="primary" onClick = {this.addLog}>
+                        Button X
+                    </Button>
                 </div>
-               
+                <div className="button2">
+                    <Button variant="contained" color="primary" onClick = {this.showLogs}>
+                        Log History
+                    </Button>
+                </div>
+
                 {this.state.logs.length === 0 ? '' : (
                     <ul>
-                       {this.state.logs.map(log => (
-                           <li>
-                               {JSON.stringify(log, null, 2)}
-                           </li>
-                       ))} 
+                        {this.state.logs.map(log => (
+                            <li>
+                                {JSON.stringify(log, null, 2)}
+                            </li>
+                        ))}
                     </ul>
                 )}
-                 <button className = "w-full py-3 bg-red-600 mt-4 text-white" onClick = {() => {auth.signOut()}}>Sign out</button>
+                <button className = "w-full py-3 bg-red-600 mt-4 text-white" onClick = {() => {auth.signOut()}}>Sign out</button>
             </div>
         );
     }
